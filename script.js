9bd377a71fda6cfabce1a86e48d07075ff3a40a0
ValeriaBodrova <110822668+ValeriaBodrova@users.noreply.github.com>
2023-06-12 11:40:46 +0200

    task2 done

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ alert(array11);
 
 array11.splice(1,3);
 alert(array11);
+
+//task2
+
+array22 = [16,-37,54,-4,72,-56,47,4,-16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
+console.log(array22)
+
+let sum = 0;
+let count = 0;
+
+
+for ( let i = 0; i < array22.length; i++){
+    if(array22[i] > 0){
+        sum += array22[i];
+        count++;
+    }
+}
+
+console.log(sum);
+console.log(count);
